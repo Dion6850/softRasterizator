e8e1e67550a56a93cd7d08c00b0fc7d3c811762c
Dion <1079894021@qq.com>
2025-07-14 14:37:35 +0800

    add resource

---
+++ include/core/Rasterizer.h
@@ -0,0 +1,27 @@
+/**
+ * @file Rasterizer.h
+ * @author dion (hduer_zdy@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025/7/13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef RASTERIZER_H
+#define RASTERIZER_H
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include "core/resource.h"
+#include "utils/MVP.h"
+namespace Rasterizer {
+
+class Rasterizer {
+public:
+  static
+};
+
+} // Rasterizer
+
+#endif //RASTERIZER_H
---
+++ src/core/Rasterizer.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file Rasterizer.h
+ * @author dion (hduer_zdy@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025/7/13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "core/Rasterizer.h"
+
+namespace Rasterizer {
+
+} // Rasterizer
